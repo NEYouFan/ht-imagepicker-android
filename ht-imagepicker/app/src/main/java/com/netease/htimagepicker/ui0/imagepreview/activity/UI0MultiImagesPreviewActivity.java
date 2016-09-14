ecf1762e556a409dcd92da7ec1cc4c1af4ad0f48
张云龙 <hzzhangyunlong@corp.netease.com>
2016-09-14 19:51:56 +0800

    添加版权信息

--- ht-imagepicker/app/src/main/java/com/netease/htimagepicker/ui0/imagepreview/activity/UI0MultiImagesPreviewActivity.java
+++ ht-imagepicker/app/src/main/java/com/netease/htimagepicker/ui0/imagepreview/activity/UI0MultiImagesPreviewActivity.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright by Netease (c) 2016.
+ * This source code is licensed under the MIT-style license found in the LICENSE file
+ *  in the root directory of this source tree.
+ */
+
 package com.netease.htimagepicker.ui0.imagepreview.activity;
 
 import android.os.Bundle;
--- ht-imagepicker/module/htimagepicker/src/main/java/com/netease/hearttouch/htimagepicker/core/util/storage/StorageUtil.java
+++ ht-imagepicker/module/htimagepicker/src/main/java/com/netease/hearttouch/htimagepicker/core/util/storage/StorageUtil.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright by Netease (c) 2016.
+ * This source code is licensed under the MIT-style license found in the LICENSE file
+ *  in the root directory of this source tree.
+ */
+
 package com.netease.hearttouch.htimagepicker.core.util.storage;
 
 import android.annotation.TargetApi;
